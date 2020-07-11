49e2ddb1691ac8c8375a361b111193f1920a3be3
Gregor Adams <greg@pixelass.com>
2020-07-11 17:57:00 +0200

    feat: adjust animation and shadow

--- src/components/cover.tsx
+++ src/components/cover.tsx
@@ -18,8 +18,8 @@ export const Hover = styled.div`
 
 export const GalleryImage = styled(Img)`
 	width: 100%;
-	transform: scale3d(1, 1, 1);
-	transition: transform 0.25s ease-in-out;
+	transform: scale3d(1.01, 1.01, 1);
+	transition: transform 2s ease-in-out;
 `;
 
 export const Cover = styled.div`
@@ -27,15 +27,17 @@ export const Cover = styled.div`
 	position: relative;
 	display: flex;
 	overflow: hidden;
-	box-shadow: 0 3px 8px rgba(0, 0, 0, 0.5);
+	box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
 	border-radius: 5px;
+	transition: box-shadow 0.25s ease-in-out;
+	will-change: box-shadow;
 	&:hover {
+		box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
 		${Hover} {
 			transform: translate3d(0, 0, 0);
 		}
 		${GalleryImage} {
 			transform: scale3d(1.1, 1.1, 1);
-			transition-duration: 1s;
 		}
 	}
 `;
